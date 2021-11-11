633813de39af4f5003fe67ea694ea9ecd3540975
ctecher <techer.charles@educ-valadon-limoges.fr>
2021-11-11 22:30:51 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿/**
+ * Exercice 4 : ajout de la fonction saisie dans un programme
+ * author : CT
+ * date : 11/2021
+ */
+using System;
+
+namespace FonctionExercice4
+{
+    class Program
+    {
+        /// <summary>
+        /// Saisie de L ou E avec contrôle de saisie
+        /// </summary>
+        /// <returns>lettre statut</returns>
+        static void Main(string[] args)
+        {
+            // Déclaration
+            char statut;
+            // Boucle sur la saisie correcte du statut
+            do
+            {
+                Console.WriteLine();
+                Console.Write("Etes-vous lycéen(ne) ou étudiant(e) ? (L/E) ");
+                statut = Console.ReadKey().KeyChar;
+            } while (statut != 'L' && statut != 'E');
+            // Affichage du message personnalisé
+            Console.WriteLine();
+            if (statut == 'L')
+            {
+                Console.WriteLine("Bonjour lycée(ne).");
+            }
+            else
+            {
+                Console.WriteLine("Bonjour étudiant(e)");
+            }
+            Console.ReadLine();
+        }
+    }
+}
